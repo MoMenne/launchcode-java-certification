e84e359eba97835bb9e33035e94ad6e6f12cbda8
MoMenne <mpmenne@electronicsystemssupport.com>
2014-06-19 20:05:22 -0500

    adding some additional flashcards in a separate file ... didnt' want to conflict

---
+++ src/com/launchcode/java/basics/OverridingMethods.java
@@ -0,0 +1,19 @@
+package com.launchcode.java.basics;
+
+/**
+ * User: mpmenne
+ * Date: 6/19/14
+ * Time: 12:20 PM
+ */
+
+class Class1 {
+    public void doSomething() throws Exception {}
+}
+
+class Class2 extends Class1 {
+    @Override
+    public void doSomething(){}
+}
+
+public class OverridingMethods {
+}
